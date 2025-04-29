b4c924495171629b1c261ceb0d5daa6466bd050d
pengamki <peeranat.0709@gmail.com>
2025-04-30 00:18:26 +0700

    structs

--- include/mlmas.h
+++ include/mlmas.h
@@ -1,8 +1,11 @@
 #ifndef MLMAS_H
 # define MLMAS_H
 
-# include <stdio.h>
 # include <stdlib.h>
 # include <time.h>
 # include <string.h>
+
+# define DOWNLINES_LIMIT 10
+# define AGENTS_LIMIT 100
+
 #endif
--- src/main.c
+++ src/main.c
@@ -1,14 +1,61 @@
 #include "../include/mlmas.h"
+#include <stdio.h>
+#include <stdbool.h>
 
-#define MAX 100
-
-typedef struct Node {
+typedef struct s_Agent {
   int agent_id;
-  struct Node **downlines;
+  struct s_Agent *upline;
+  struct s_Agent *downlines[DOWNLINES_LIMIT];
   int downlines_count;
-} Node;
+  float personal_sales; // Total sales
+  floar commission_earning; // Earning from commission
+  float personal_commission_rate; // Percentage of commission earned from sales
+  float downline_commission_rate; // Percentage of commission earced from downlines
+} t_Agent;
+
+typedef struct s_HashTable {
+  int agent_id;
+  t_Agent *agent_node;
+  bool occupied;
+} t_HashTable;
+
+void init_table(t_HashTable table)
+{
+  for (int i = 0; i < AGENTS_LIMIT; i++)
+  {
+    table[i].occupied = true;
+    table[i].agent_node = NULL;
+  }
+}
 
 int main(void)
 {
-  //  
+  t_Agent *you;
+  t_HashTable agents_table[AGENTS_LIMIT];
+  int running = 1;
+  int choice;
+
+  init_table(agents_table);
+  while (running)
+  {
+    printf("[Welcome]");
+    printf("1. -\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+      case 0:
+        printf("option 0 selected\n");
+        printf("[Exit]");
+        running = 0;
+        break;
+      case 1:
+        printf("option 1 selected\n");
+        //
+        break;
+      default:
+        printf("invalid option");
+        break;
+    }
+  }
 }
